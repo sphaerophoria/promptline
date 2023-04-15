3aac59d355f2c692802a578ce2dfc9ada32d4aee
Mick Sayson <mick@sayson.com>
2023-04-15 12:25:02 -0700

    Reduce dependencies
    Installation should be as simple as possible. As this is one of the
    first programs I install on a new system, the friction of having to
    install cmake/pkg-config/openssl just to get my promptline compiled is a
    pain. Remove expensive dependency (libgit2) and replace with manual
    parsing of git info. While we're in the mindset of reducing
    dependencies, remove dependency on anyhow and fall back to more bare
    bones style rust error handling

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,47 @@
-use anyhow::{anyhow, bail, Context, Result};
-use colored::{Colorize, ColoredString};
-use std::{env, fs::File, io::{self, Read, Write}, path::PathBuf};
+use colored::{ColoredString, Colorize};
 
-fn get_time() -> Result<ColoredString> {
+use std::{
+    env,
+    error::Error,
+    fmt,
+    fs::{self, File},
+    io::{self, Read, Write},
+    path::{Path, PathBuf},
+};
+
+fn get_time() -> ColoredString {
     let time = chrono::Local::now().time();
-    Ok(format!("{}", time.format("%H:%M")).cyan().bold())
+    format!("{}", time.format("%H:%M")).cyan().bold()
 }
 
-fn get_user() -> Result<ColoredString> {
+#[derive(Debug)]
+enum UserError {
+    GetUser(nix::Error),
+    NoUser,
+}
+
+impl fmt::Display for UserError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            UserError::GetUser(_) => write!(f, "failed to get user"),
+            UserError::NoUser => write!(f, "no active user"),
+        }
+    }
+}
+
+impl Error for UserError {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        match self {
+            UserError::GetUser(e) => Some(e),
+            UserError::NoUser => None,
+        }
+    }
+}
+
+fn get_user() -> Result<ColoredString, UserError> {
     let user = nix::unistd::User::from_uid(nix::unistd::getuid())
-        .into_iter()
-        .flatten()
-        .next()
-        .ok_or(anyhow!("Failed to get user"))?;
+        .map_err(UserError::GetUser)?
+        .ok_or(UserError::NoUser)?;
 
     let color_user = match user.name.as_str() {
         "root" => user.name.red().bold(),
@@ -22,35 +51,71 @@ fn get_user() -> Result<ColoredString> {
     Ok(color_user)
 }
 
-fn get_hostname() -> Result<ColoredString> {
+#[derive(Debug)]
+enum HostnameError {
+    GetHostname(nix::Error),
+    GetHostnameString(std::str::Utf8Error),
+}
+
+impl fmt::Display for HostnameError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            HostnameError::GetHostname(_) => write!(f, "failed to get host name"),
+            HostnameError::GetHostnameString(_) => {
+                write!(f, "failed to convert hostname to string")
+            }
+        }
+    }
+}
+
+impl Error for HostnameError {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        match self {
+            HostnameError::GetHostname(e) => Some(e),
+            HostnameError::GetHostnameString(e) => Some(e),
+        }
+    }
+}
+
+fn get_hostname() -> Result<ColoredString, HostnameError> {
     let mut buf = [0u8; 64];
     let res = nix::unistd::gethostname(&mut buf)
-        .context("Failed to retrieve hostname")?
+        .map_err(HostnameError::GetHostname)?
         .to_str()
-        .context("Hostname not a valid utf8 string")?
+        .map_err(HostnameError::GetHostnameString)?
         .green()
         .bold();
 
     Ok(res)
 }
 
-fn get_status() -> Result<ColoredString> {
-    let status = env::args().nth(1).ok_or(anyhow!("No exit status"))?;
+#[derive(Debug)]
+struct NoExitStatus;
 
-    let color_status  = match status.as_str() {
+impl fmt::Display for NoExitStatus {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "no exit status")
+    }
+}
+
+impl Error for NoExitStatus {}
+
+fn get_status() -> Result<ColoredString, NoExitStatus> {
+    let status = env::args().nth(1).ok_or(NoExitStatus)?;
+
+    let color_status = match status.as_str() {
         "0" => status.green().bold(),
-        _ => status.red().bold()
+        _ => status.red().bold(),
     };
 
     Ok(color_status)
 }
 
-fn get_cwd() -> Result<ColoredString> {
-
+fn get_cwd() -> ColoredString {
     let cwd = env::var("PWD");
 
     if cwd.is_err() {
-        return Ok("!!!".red().bold());
+        return "!!!".red().bold();
     }
 
     let mut cwd = cwd.unwrap();
@@ -61,11 +126,35 @@ fn get_cwd() -> Result<ColoredString> {
         }
     }
 
-    Ok(cwd.blue().bold())
+    cwd.blue().bold()
 }
 
-fn get_mercurial_info() -> Result<ColoredString> {
-    let mut hg_root = env::current_dir().with_context(|| "No cwd")?;
+#[derive(Debug)]
+enum HgError {
+    NoCwd(std::io::Error),
+    NotHg,
+}
+
+impl fmt::Display for HgError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            HgError::NoCwd(_) => write!(f, "failed to get working directory"),
+            HgError::NotHg => write!(f, "working directory not in hg repo"),
+        }
+    }
+}
+
+impl Error for HgError {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        match self {
+            HgError::NoCwd(e) => Some(e),
+            HgError::NotHg => None,
+        }
+    }
+}
+
+fn get_mercurial_info() -> Result<ColoredString, HgError> {
+    let mut hg_root = env::current_dir().map_err(HgError::NoCwd)?;
 
     loop {
         if hg_root.join(".hg").exists() {
@@ -73,7 +162,7 @@ fn get_mercurial_info() -> Result<ColoredString> {
         }
 
         if !hg_root.pop() {
-            bail!("Not in hg folder");
+            return Err(HgError::NotHg);
         }
     }
 
@@ -83,7 +172,7 @@ fn get_mercurial_info() -> Result<ColoredString> {
         let mut maybe_push_file = |path| {
             // Don't care if this fails, just don't include it in the results
             let _ = File::open(hg_root.join(path))
-                .and_then(|mut f|{
+                .and_then(|mut f| {
                     let mut output = String::new();
                     f.read_to_string(&mut output)?;
                     Ok(output)
@@ -100,7 +189,7 @@ fn get_mercurial_info() -> Result<ColoredString> {
         .flat_map(|f| f.bytes())
         .take(6)
         .filter_map(|x| x.ok())
-        .map(|x| format!("{:02x}", x))
+        .map(|x| format!("{x:02x}"))
         .collect::<String>();
 
     hg_components.push(s);
@@ -108,11 +197,13 @@ fn get_mercurial_info() -> Result<ColoredString> {
     let mut output = String::new();
 
     for (i, component) in hg_components.iter().enumerate() {
-        if component.is_empty() { continue; }
+        if component.is_empty() {
+            continue;
+        }
         if i != 0 {
             output.push(' ')
         }
-        output.push_str(&component);
+        output.push_str(component);
     }
 
     if output.is_empty() {
@@ -123,100 +214,290 @@ fn get_mercurial_info() -> Result<ColoredString> {
     Ok(output)
 }
 
-fn get_git_info() ->  Result<ColoredString>{
-    use git2::Repository;
-    let get_git_name  = || -> std::result::Result<String, git2::Error> {
-        let repo = Repository::discover(".")?;
-        let head = repo.head()?;
-        let name = head.shorthand().ok_or(git2::Error::from_str("Failed to get name"))?;
-        let oid = head.target().ok_or(git2::Error::from_str("Failed to get target"))?;
-        let oid_str = oid.as_bytes().iter().take(6).map(|x| format!("{:02x}", x)).collect::<String>();
-        Ok(format!("{} {}", name, oid_str))
-    };
+#[derive(Debug)]
+enum GitError {
+    NoCwd(std::io::Error),
+    CanonicalCwd(std::io::Error),
+    ReadGitFile(std::io::Error),
+    ReadHead(std::io::Error),
+    NotGitRepo,
+    UnexpectedGitContent,
+    ReadRef(std::io::Error),
+    NoRefName,
+}
 
-    match get_git_name() {
-      Ok(s) => Ok(s.green().bold()),
-      Err(_) => bail!("Not in git folder"),
+impl fmt::Display for GitError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            GitError::NoCwd(_) => write!(f, "failed to get cwd"),
+            GitError::CanonicalCwd(_) => write!(f, "failed to canonicalize cwd"),
+            GitError::ReadGitFile(_) => write!(f, "failed to read .git file"),
+            GitError::ReadHead(_) => write!(f, "failed to read git HEAD"),
+            GitError::NotGitRepo => write!(f, "not a git repo"),
+            GitError::UnexpectedGitContent => write!(f, "unexpected git content"),
+            GitError::ReadRef(_) => write!(f, "failed to read ref"),
+            GitError::NoRefName => write!(f, "failed to get ref name"),
+        }
     }
 }
 
-fn get_conda_info() -> Result<ColoredString> {
-    let conda_env = match std::env::var("CONDA_DEFAULT_ENV") {
-        Ok(v) => v,
-        Err(_) => bail!("No conda env")
-    };
-
-    Ok(format!("🐍 {}", conda_env).bold())
+impl Error for GitError {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        match self {
+            GitError::NoCwd(e) => Some(e),
+            GitError::CanonicalCwd(e) => Some(e),
+            GitError::ReadGitFile(e) => Some(e),
+            GitError::ReadHead(e) => Some(e),
+            GitError::NotGitRepo => None,
+            GitError::UnexpectedGitContent => None,
+            GitError::ReadRef(e) => Some(e),
+            GitError::NoRefName => None,
+        }
+    }
 }
 
-fn get_docker_env() -> Result<ColoredString> {
+fn get_git_info() -> Result<ColoredString, GitError> {
+    let cwd = env::current_dir().map_err(GitError::NoCwd)?;
+    let canonical_cwd = fs::canonicalize(cwd).map_err(GitError::CanonicalCwd)?;
+
+    let mut dir_iter = Some(&canonical_cwd as &Path);
+    while let Some(dir) = dir_iter {
+        if dir.join(".git").exists() {
+            break;
+        }
+
+        dir_iter = dir.parent();
+    }
+
+    let repo = dir_iter.ok_or(GitError::NotGitRepo)?;
+
+    // if .git has gitdir:.... we have to follow the link
+
+    let mut git_dir = repo.join(".git");
+    if git_dir.is_file() {
+        let git_content = fs::read_to_string(git_dir).map_err(GitError::ReadGitFile)?;
+
+        const PREFIX: &str = "gitdir: ";
+
+        match git_content.strip_prefix(PREFIX) {
+            Some(v) => git_dir = v.trim().into(),
+            None => return Err(GitError::UnexpectedGitContent),
+        }
+    }
+
+    let head_content = fs::read_to_string(git_dir.join("HEAD")).map_err(GitError::ReadHead)?;
+
+    const REF_PREFIX: &str = "ref: ";
+    let output = match head_content.strip_prefix(REF_PREFIX) {
+        Some(refs_path) => {
+            let refs_path = Path::new(refs_path.trim());
+
+            let commit_hash = fs::read_to_string(git_dir.join(refs_path)).map_err(GitError::ReadRef)?;
+
+            let short_hash = &commit_hash[..14];
+            let ref_name = refs_path
+                .file_name()
+                .ok_or(GitError::NoRefName)?
+                .to_string_lossy();
+
+            format!("{ref_name} {short_hash}")
+        }
+        None => {
+            head_content[..14].to_string()
+        }
+    };
+
+    Ok(output.green().bold())
+}
+
+#[derive(Debug)]
+struct NoCondaEnv;
+
+impl fmt::Display for NoCondaEnv {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "no conda env var")
+    }
+}
+
+impl Error for NoCondaEnv {}
+
+fn get_conda_info() -> Result<ColoredString, NoCondaEnv> {
+    let conda_env = std::env::var("CONDA_DEFAULT_ENV").map_err(|_| NoCondaEnv)?;
+    Ok(format!("🐍 {conda_env}").bold())
+}
+
+#[derive(Debug)]
+struct NotDockerContainer;
+
+impl fmt::Display for NotDockerContainer {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "not docker container")
+    }
+}
+
+impl Error for NotDockerContainer {}
+
+fn get_docker_env() -> Result<ColoredString, NotDockerContainer> {
     match std::fs::metadata("/.dockerenv") {
         Ok(_) => Ok("🐳".into()),
-        Err(_) => bail!("Not in docker container")
+        Err(_) => Err(NotDockerContainer),
     }
 }
 
-fn get_shell() -> Result<ColoredString> {
+#[derive(Debug)]
+enum ShellError {
+    EnvNotSet,
+    NoShellName,
+}
+
+impl fmt::Display for ShellError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            ShellError::EnvNotSet => write!(f, "shell env not set"),
+            ShellError::NoShellName => write!(f, "failed to get shell name"),
+        }
+    }
+}
+
+impl Error for ShellError {}
+
+fn get_shell() -> Result<ColoredString, ShellError> {
     let shell: PathBuf = std::env::var("SHELL")
-        .context("SHELL not set")?
+        .map_err(|_| ShellError::EnvNotSet)?
         .into();
 
-    let name = shell.file_name()
-        .ok_or(anyhow!("Failed to get shell name"))?
-        .to_str()
-        .ok_or(anyhow!("Failed to convert shell name to string"))?;
+    let name = shell
+        .file_name()
+        .ok_or(ShellError::NoShellName)?
+        .to_string_lossy();
 
     Ok(name.bold())
 }
 
-fn show_nix_shell() -> Result<ColoredString> {
+#[derive(Debug)]
+struct NotInNixShell;
 
-    std::env::var("IN_NIX_SHELL")
-        .context("Not in nix shell")?;
+impl fmt::Display for NotInNixShell {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "not in nix shell")
+    }
+}
 
-    let shell_name = std::env::var("name")
-        .unwrap_or("nix-shell".to_string());
+impl Error for NotInNixShell {}
 
-    Ok(format!("nix: {}", shell_name).bold())
+fn show_nix_shell() -> Result<ColoredString, NotInNixShell> {
+    std::env::var("IN_NIX_SHELL").map_err(|_| NotInNixShell)?;
+
+    let shell_name = std::env::var("name").unwrap_or("nix-shell".to_string());
+
+    Ok(format!("nix: {shell_name}").bold())
 }
 
 fn do_print(mut components: Vec<ColoredString>) {
-    components.insert(0,"┌[".into());
+    components.insert(0, "┌[".into());
     for i in 1..components.len() - 1 {
-        components.insert(2*i, "]-[".into());
+        components.insert(2 * i, "]-[".into());
     }
     components.push("]\n└> ".into());
     for component in components {
-        print!("{}", component);
+        print!("{component}");
     }
 }
 
-fn main() -> Result<()> {
+#[derive(Debug)]
+enum MainError {
+    Docker(NotDockerContainer),
+    User(UserError),
+    Hostname(HostnameError),
+    Shell(ShellError),
+    Status(NoExitStatus),
+    Mercurial(HgError),
+    Git(GitError),
+    Conda(NoCondaEnv),
+    NixShell(NotInNixShell),
+}
+
+impl fmt::Display for MainError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let source: &dyn Error = match self {
+            MainError::Docker(e) => {
+                writeln!(f, "failed to get docker info")?;
+                e
+            }
+            MainError::User(e) => {
+                writeln!(f, "failed to get user info")?;
+                e
+            }
+            MainError::Hostname(e) => {
+                writeln!(f, "failed to get hostname info")?;
+                e
+            }
+            MainError::Shell(e) => {
+                writeln!(f, "failed to get shell info")?;
+                e
+            }
+            MainError::Status(e) => {
+                writeln!(f, "failed to get exit status")?;
+                e
+            }
+            MainError::Mercurial(e) => {
+                writeln!(f, "failed to get mercurial info")?;
+                e
+            }
+            MainError::Git(e) => {
+                writeln!(f, "failed to get git info")?;
+                e
+            }
+            MainError::Conda(e) => {
+                writeln!(f, "failed to get conda info")?;
+                e
+            }
+            MainError::NixShell(e) => {
+                writeln!(f, "failed to get nix shell info")?;
+                e
+            }
+        };
+
+        writeln!(f, "Caused by:")?;
+
+        let mut source = Some(source);
+        while let Some(err) = source {
+            writeln!(f, "{err}")?;
+            source = err.source();
+        }
+
+        Ok(())
+    }
+}
+
+fn main() {
     colored::control::set_override(true);
 
-    let (oks, errors): (Vec<Result<_>>, _) = vec![
-        get_time(),
-        get_docker_env(),
-        get_user(),
-        get_hostname(),
-        get_cwd(),
-        get_shell(),
-        get_status(),
-        get_mercurial_info(),
-        get_git_info(),
-        get_conda_info(),
-        show_nix_shell(),
+    let (oks, errors): (Vec<_>, Vec<_>) = vec![
+        Ok(get_time()),
+        get_docker_env().map_err(MainError::Docker),
+        get_user().map_err(MainError::User),
+        get_hostname().map_err(MainError::Hostname),
+        Ok(get_cwd()),
+        get_shell().map_err(MainError::Shell),
+        get_status().map_err(MainError::Status),
+        get_mercurial_info().map_err(MainError::Mercurial),
+        get_git_info().map_err(MainError::Git),
+        get_conda_info().map_err(MainError::Conda),
+        show_nix_shell().map_err(MainError::NixShell),
     ]
     .into_iter()
-    .partition(|x| x.is_ok());
+    .partition(Result::is_ok);
 
-    let components: Vec<_> = oks.into_iter().map(|x| x.unwrap()).collect();
+    let components: Vec<_> = oks
+        .into_iter()
+        .map(|x| x.expect("Invalid result"))
+        .collect();
 
     if Ok("1") == env::var("DEBUG_PROMPTLINE").as_ref().map(|s| s.as_str()) {
         for error in errors.into_iter().map(|e| e.unwrap_err()) {
-            let _ = write!(io::stderr(), "{:?}\n", error);
+            let _ = writeln!(io::stderr(), "{error}");
         }
     }
     do_print(components);
-    Ok(())
 }
